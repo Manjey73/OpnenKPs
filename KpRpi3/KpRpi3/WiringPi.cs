f08e4d76bfbb1cc6d809a54cad7733219559e37b
Manjey73 <aburakhin@bk.ru>
2017-05-24 10:43:09 +0300

    Update WiringPi.cs

--- KpRpi3/KpRpi3/WiringPi.cs
+++ KpRpi3/KpRpi3/WiringPi.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Scada.Comm.Devices.Enums;
 using System.Runtime.InteropServices;
 
 namespace Scada.Comm.Devices
