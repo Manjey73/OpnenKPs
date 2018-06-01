3bc9462d0334f22a774f224a6822bfc2188ea947
Manjey73 <aburakhin@bk.ru>
2018-06-01 11:18:26 +0300

    Update BitFunc.cs

--- ScadaCommFunc/ScadaCommFunc/BitFunc.cs
+++ ScadaCommFunc/ScadaCommFunc/BitFunc.cs
@@ -1,32 +1,57 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScadaCommFunc
 {
-    public class BitFunc
+    public static class BitFunc
     {
-        public static UInt32 ROL(UInt32 number, int shift) // цикличиский сдвиг битов влево до 32-х бит, на входе переменная UInt32
+        /// <summary>
+        /// цикличиский сдвиг битов влево до 32-х бит, на входе переменная UInt32
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="shift"></param>
+        /// <returns></returns>
+        /// 
+        public static UInt32 ROL(UInt32 number, int shift)
         {
             shift %= 31;
             return ((number << shift) | (number >> (32 - shift)));
         }
 
-        public static UInt32 ROR(UInt32 number, int shift) // цикличиский сдвиг битов вправо до 32-х бит, на входе переменная UInt32
+        /// <summary>
+        /// цикличиский сдвиг битов вправо до 32-х бит, на входе переменная UInt32
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="shift"></param>
+        /// <returns></returns>
+
+        public static UInt32 ROR(UInt32 number, int shift)
         {
             shift %= 31;
             return ((number >> shift) | (number << (32 - shift)));
         }
 
-        public static int GetBit(int val, int n) // Получить бит
+        /// <summary>
+        /// Получить бит из числа int
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// 
+        public static int GetBit(int val, int n)
         {
             int intVal = val;
             return (intVal >> n) & 1;
         }
 
-        public static int SetBit(int n, int index, bool value) // вставить бит
+        /// <summary>
+        /// вставить бит в чисто int
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// 
+        public static int SetBit(int n, int index, bool value) 
         {
             return value ? n | (1 << index) : n & ~(1 << index);
         }
