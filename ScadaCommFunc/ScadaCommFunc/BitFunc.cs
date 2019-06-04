357d12d6b28f85bd1d5ffa4f0357c093eb49a500
Manjey73 <aburakhin@bk.ru>
2019-06-04 14:13:51 +0300

    Update BitFunc.cs

--- ScadaCommFunc/ScadaCommFunc/BitFunc.cs
+++ ScadaCommFunc/ScadaCommFunc/BitFunc.cs
@@ -44,7 +44,7 @@ namespace ScadaCommFunc
         }
 
         /// <summary>
-        /// вставить бит в чисто int
+        /// вставить бит в число int
         /// </summary>
         /// <param name="n"></param>
         /// <param name="index"></param>
@@ -54,6 +54,22 @@ namespace ScadaCommFunc
         public static int SetBit(int n, int index, bool value) 
         {
             return value ? n | (1 << index) : n & ~(1 << index);
+        }
+
+        /// <summary>
+        /// Расчитать количество бит в 32-х битном числе
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// 
+        public static int CountBit32(int n)
+        {
+            n -= (n >> 1) & 0x55555555;
+            n = ((n >> 2) & 0x33333333) + (n & 0x33333333);
+            n = ((n >> 4) + n) & 0x0F0F0F0F;
+            n = ((n >> 8) + n) & 0x00FF00FF;
+            n = ((n >> 16) + n);
+            return n;
         }
     }
 }
