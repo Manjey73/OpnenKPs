625ecf2b964d675967ba6e2aec2a599afe74a26d
Manjey73 <aburakhin@bk.ru>
2019-06-05 22:40:44 +0300

    Update BitFunc.cs

--- ScadaCommFunc/ScadaCommFunc/BitFunc.cs
+++ ScadaCommFunc/ScadaCommFunc/BitFunc.cs
@@ -64,11 +64,11 @@ namespace ScadaCommFunc
         /// 
         public static int CountBit32(int n)
         {
-            n -= (n >> 1) & 0x55555555;
+            n = ((n >> 1) & 0x55555555) + (n & 0x55555555);
             n = ((n >> 2) & 0x33333333) + (n & 0x33333333);
-            n = ((n >> 4) + n) & 0x0F0F0F0F;
-            n = ((n >> 8) + n) & 0x00FF00FF;
-            n = ((n >> 16) + n);
+            n = ((n >> 4) & 0x0F0F0F0F) + (n & 0x0F0F0F0F);
+            n = ((n >> 8) & 0x00FF00FF) + (n & 0x00FF00FF);
+            n = ((n >> 16) & 0x0000FFFF) + (n & 0x0000FFFF);
             return n;
         }
     }
