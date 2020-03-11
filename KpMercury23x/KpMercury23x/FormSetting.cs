967488bae9f6e93e3a770d383c92e45699210375
Manjey73 <aburakhin@bk.ru>
2020-03-11 22:25:56 +0300

    Update FormSetting.cs

--- KpMercury23x/KpMercury23x/FormSetting.cs
+++ KpMercury23x/KpMercury23x/FormSetting.cs
@@ -79,6 +79,7 @@ namespace Scada.Comm.Devices.KpMercury23x
 
             kpProps.Modified = false;
 
+
             // настройка элементов управления
             openFileDialog.InitialDirectory = appDirs.ConfigDir;
 
