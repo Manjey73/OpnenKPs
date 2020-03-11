6213642ea1912f3762a72d6e38698f25761e0352
Manjey73 <aburakhin@bk.ru>
2020-03-11 22:22:00 +0300

    Update KpMercury23xView.cs

--- KpMercury23x/KpMercury23x/KpMercury23xView.cs
+++ KpMercury23x/KpMercury23x/KpMercury23xView.cs
@@ -1,28 +1,79 @@
+/*
+ * Copyright 2020 Andrey Burakhin
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpMercury23x
+ * Summary  : Device communication logic
+ * 
+ * Author   : Andrey Burakhin
+ * Created  : 2017
+ * Modified : 2020
+ */
+
+
 using Scada.Comm.Devices.KpMercury23x;
 using Scada.Data;
 using System;
+using System.IO;
 using System.Collections.Generic;
+using ScadaCommFunc;
+using Scada.Data.Configuration;
 using System.Text;
 
 namespace Scada.Comm.Devices
 {
     public sealed class KpMercury23xView : KPView
     {
+        private DevTemplate devTemplate = new DevTemplate();
+
+        private class ActiveCnlList
+        {
+            public int Cnl { get; set; }                // Cnl       - Номер активного сигнала
+            public string Name { get; set; }            // Name      - Имя активного сигнала
+            public bool cmdActive { get; set; }         // cmdActive - true, у канала есть активная команда
+            public int idGroup { get; set; }         // Индекс группы, которой принадлежит Значение
+        }
+
+        private List<ActiveCnlList> ActiveCnl = new List<ActiveCnlList>(); // Создание списка Активных сигналов, где ActiveCnl.Cnl - номер сигнала, ActiveCnl.Name - Имя сигнала, 
+        private string Allname;         // Полное имя тега
+        private bool activeuse = false;
 
 
         public override string KPDescr
         {
             get
             {
-                return 
+                return
                 "Автор Бурахин Андрей, email: aburakhin@bk.ru\n\n" +
                 "Опрос электросчётчика Меркурий (230, 231, 232, 233, 236).\n\n" +
                 "Адрес счетчика  = Администратор -> КП -> Адрес\n\n" +
 
-                "Командная строка Коммуникатора, параметр должен заканчиваться разделителем ;\n" +
-                "1-й параметр = Пароль счетчика 6 цифр\n2-й параметр = Уровень доступа 1 или 2\n" +
-                "3-й параметр = битовая маска считываемых параметров\n" +
-                "бит числа, выставленный в 1 разрешает чтение группы параметров\n\n" +
+                "Параметры DevTamplate:\npassword - пароль счетчика (asccii символы)\nreadparam - '14h' или '16h'\n" +
+                "mode - '1' или '2'\nlog - false или true (логирование в журнале)\n\n" +
+                "Параметры SndGroups - SndRequest:\n" +
+
+                "Name - общее имя группы параметров\n" +
+                "Active - активность чтения группы параметров\n" +
+                "Bit - Номер бита для группы параметров (см. ниже)\n\n" +
+                "Список параметров value:\n" +
+                "name - имя обособленного параметра \n" +
+                "signal - номер сигнала для параметра \n" +
+                "active - (*) активнось параметра \n" +
+                "range  - (*) множитель параметра \n" +
+                "(*) - зарезервировано\n\n" +
+
                 "    Мгновенные значения:\n\n" +
                 "bit 0 - Мощность P ∑, L1, L2, L3\n" +
                 "bit 1 - Мощность Q ∑, L1, L2, L3\n" +
@@ -42,15 +93,7 @@ namespace Scada.Comm.Devices
                 "bit 14 - Тариф 1   А+ L1, L2, L3\n" +
                 "bit 15 - Тариф 2   А+ L1, L2, L3\n" +
                 "bit 16 - Тариф 3   А+ L1, L2, L3\n" +
-                "bit 16 - Тариф 4   А+ L1, L2, L3\n\n" +
-                "Пример: 255 - все значения мгновенных мощностей\n" +
-                "191 - значения мгновенных мощностей без углов м-ду ф.\n" +
-                "261888 - все значения энергии от сброса\n" +
-                "59136  - значения энергии от сброса без тарифов 3 и 4\n\n" +
-                "Пример командной строки - 111111;1;191; - пароль 111111, уровень доступа 1, читать\n" +
-                "значения мгновенных значений без углов между фазами\n" +
-                ";;191 = пароль по умолчанию 111111, уровень доступа по умолчанию 1\n" +
-                "Пустая строка пароль по умолчанию 111111, уровень доступа 1, все параметры 262143";
+                "bit 17 - Тариф 4   А+ L1, L2, L3";
             }
         }
 
@@ -63,7 +106,6 @@ namespace Scada.Comm.Devices
             CanShowProps = true;
         }
 
-
         // Получить параметры опроса КП по умолчанию
         public override KPReqParams DefaultReqParams
         {
@@ -73,11 +115,130 @@ namespace Scada.Comm.Devices
             }
         }
 
-
         // Отобразить свойства КП
         public override void ShowProps()
         {
             FormSetting.ShowDialog(Number, KPProps, AppDirs); // отображение фармы параметров
         }
+
+        /// <summary>
+        /// Gets the channel prototypes.
+        /// </summary>
+        public override KPCnlPrototypes DefaultCnls
+        {
+            get
+            {
+                devTemplate = null;
+
+                KPCnlPrototypes prototypes = new KPCnlPrototypes();
+                List<InCnlPrototype> inCnls = prototypes.InCnls;
+                List<CtrlCnlPrototype> ctrlCnls = prototypes.CtrlCnls;
+
+                // загрузка шаблона устройства
+                string fileName = KPProps == null ? "" : KPProps.CmdLine.Trim();
+
+                if (fileName == "")
+                    return null;
+                string filePath = Path.IsPathRooted(fileName) ? fileName : Path.Combine(AppDirs.ConfigDir, fileName);
+
+                try
+                {
+                    devTemplate = FileFunc.LoadXml(typeof(DevTemplate), filePath) as DevTemplate;
+                }
+                catch (Exception ex)
+                {
+                    throw new ScadaException(string.Format(Localization.UseRussian ?
+                    "Ошибка при получении типа логики КП из библиотеки {0}" :
+                    "Error getting device logic type from the library {0}", ex.Message), ex);
+                }
+
+                // Проверка на наличие конфигурации XML
+                if (devTemplate != null)
+                {
+                    ActiveCnl.Clear(); // Очищаем список, так как код срабатывает при выборе КП при Создании каналов каждый раз...
+
+                    if (devTemplate.SndGroups.Count != 0) // Определить активные запросы объектов и записать в список индексы запросов для создания тегов
+                    {
+                        for (int sg = 0; sg < devTemplate.SndGroups.Count; sg++)
+                        {
+                            if (devTemplate.SndGroups[sg].Active)
+                            {
+                                if (devTemplate.SndGroups[sg].value.Count > 0)
+                                {
+                                    for (int y = 0; y < devTemplate.SndGroups[sg].value.Count; y++)
+                                    {
+                                        if (devTemplate.SndGroups[sg].value[y].active)
+                                        {
+                                            Allname = string.Concat(devTemplate.SndGroups[sg].Name, " ", devTemplate.SndGroups[sg].value[y].name);
+                                            ActiveCnl.Add(new ActiveCnlList()
+                                            {
+                                                Cnl = devTemplate.SndGroups[sg].value[y].signal,
+                                                Name = Allname,
+                                                idGroup = devTemplate.SndGroups[sg].Bit // ID Группы является значение Bit
+                                            });
+                                            activeuse = true; // Есть активные группы запросов значений
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (activeuse)
+                    {
+                        ActiveCnl.Add(new ActiveCnlList()
+                        {
+                            Cnl = 70,
+                            Name = "Код ошибки:"
+                        });
+
+                        ActiveCnl.Add(new ActiveCnlList()
+                        {
+                            Cnl = 71,
+                            Name = "Коэфф. тр-ра тока"
+                        });
+
+                        ActiveCnl.Add(new ActiveCnlList()
+                        {
+                            Cnl = 72,
+                            Name = "Коэфф. тр-ра напряжения"
+                        });
+
+                        // ------------ Тут формирование списка каналов управления для Администратора ------------ ЗАРЕЗЕРВИРОВАНО
+                        // output channels
+                        //foreach (var listCmd in ActiveCnl)
+                        //{
+                        //    if (listCmd.cmdActive) // Если у канала есть активная команда, значение cmdActive = true, добавляем ее в список прототипов команд
+                        //    {
+                        //        ctrlCnls.Add(new CtrlCnlPrototype("Уст. " + listCmd.Name, BaseValues.CmdTypes.Standard)
+                        //        {
+                        //            CmdNum = listCmd.Cnl
+                        //        });
+                        //    }
+                        //}
+
+                        // ------------ Тут формирование списка входных каналов для Администратора ------------
+                        // ------------      с учетом подготовленного списка каналов управления        ------------
+                        // input channels
+
+                        foreach (var listCnl in ActiveCnl)
+                        {
+                            int idx = ctrlCnls.FindIndex(x => x.CmdNum == listCnl.Cnl);
+                            inCnls.Add(new InCnlPrototype(listCnl.Name, BaseValues.CnlTypes.TI)
+                            {
+                                Signal = listCnl.Cnl,               // номера каналов управления CmdCnl и сигнала SigCnl должны совпадать при формировании шаблона
+                                CtrlCnlProps = idx != -1 ? ctrlCnls[idx] : null
+                            });                                     // Если индекс найден, добавляем к сигналу связь на команду управления
+                        }
+
+                    }
+
+                }
+                // создание прототипов каналов КП
+                return prototypes;
+            }
+        }
+
     }
 }
