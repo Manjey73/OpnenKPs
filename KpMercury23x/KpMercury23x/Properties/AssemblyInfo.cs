2f31a7cec6776cbb979023fa99b466ccbe1dd863
Manjey73 <aburakhin@bk.ru>
2020-05-08 20:01:32 +0300

    Update AssemblyInfo.cs

--- KpMercury23x/KpMercury23x/Properties/AssemblyInfo.cs
+++ KpMercury23x/KpMercury23x/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.0.1")]
-[assembly: AssemblyFileVersion("5.1.0.1")]
+[assembly: AssemblyVersion("5.1.0.2")]
+[assembly: AssemblyFileVersion("5.1.0.2")]
 
 //
